c82a3573d3edd56e3e3302dbc0aa38ea910d9f82
Manh <nguyenduymanh0406@gmail.com>
2017-07-30 02:01:49 +0700

    first commit

---
+++ src/components/TrackList.js
@@ -0,0 +1,11 @@
+import React,{Component, PropTypes} from 'react';
+
+export default class TrackList extends Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TrackList from './components/TrackList';
+
+ReactDOM.render(
+  <TrackList />,
+  document.getElementById('app')
+);
